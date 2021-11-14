c97d23bd5c4b9f5efdb0b90bcb2326665d8d9fb3
Szabo Bence <Szabo.Bence@nn.hu>
2021-11-14 14:49:15 +0100

    Add missed files

---
+++ Szakdolgozat/python-bevezetohoz-scriptek/function_template.py
@@ -0,0 +1,4 @@
+def pelda_function(parametek):
+    #script egy tabbal jobbra
+
+    return None
---
+++ Szakdolgozat/python-bevezetohoz-scriptek/while_template.py
@@ -0,0 +1,6 @@
+feltetel = 10
+i = 0
+while i < feltetel:
+    print(i)
+    i += 1
+    # 0-tol 9-ig kiirja a szamokat, lepesenkent megno egyyel az i
