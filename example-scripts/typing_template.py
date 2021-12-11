1f6719dfcd99f8820847acf0f653c688d1dee97c
Szabó,  (Bence Dániel) <szabo.bence@nn.hu>
2021-12-11 22:09:14 +0100

    code quality upgrade

--- example-scripts/typing_template.py
+++ example-scripts/typing_template.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def pelda_fuggveny(parameter1: str,paramter2: dict,parameter3: pd.DataFrame):
+def pelda_fuggveny(parameter1: str,
+                   paramter2: dict,
+                   parameter3: pd.DataFrame):
     #script
 
     return None
--- functions.py
+++ functions.py
@@ -1,17 +1,38 @@
 import numpy as np
 
-def trapez_method(x: np.array,
+
+def trapez_method(interval: np.array,
                   func: str,
                   func_params: np.array) -> float:
-    y = globals()[func](x, func_params)
-    s = 0
-    for i in range(len(x) - 1):
-        s = 0.5 * (y[i] + y[i + 1]) * (x[i + 1] - x[i])
-    return s
+    """
+    Ez a fuggveny egy intervallumra elvegzi a trapez terulet szamitast. Figyeljunk arra, hogy celszeru minnel kisebb
+    intervallumra alkalmazni a metodust.
+    :param interval: Tomb, amely tartalmazza az intervallum kezdo es vegpontjat
+    :param func: Fuggveny neve, amelyet integralunk
+    :param func_params: Fuggveny parameterei
+    :return: Trapez terulete
+    """
+    y = globals()[func](interval, func_params)
+
+    return (interval[1] - interval[0]) * (y[1] - y[0]) / 2
+
 
 def squared(x: np.array,
             params: np.array) -> float:
+    """
+    Ez a fuggveny x minden elemehez hozzarendeli a params altal megadott masodfoku fuggveny erteket
+    :param x: Lista, amely minden elemere elvegezzuk a negyzetes fuggvenyt
+    :param params: Az a lista, amelybol letrehozzuk a masodfoku fuggvenyt
+    :return: np.ndarray tipusu lista a fent leirt muveletek utan
+    """
     a = params[0]
     b = params[1]
     c = params[2]
-    return np.power(a * x, 2) + b * x + c
+    return np.power(np.multiply(a, x), 2) + np.multiply(b, x) + c
+
+
+if __name__ == "__main__":
+    print(trapez_method(np.array([0, 1]), "squared", [1, 2, 1]))
+    print(squared([0], [1, 2, 1]))
+    print(squared([0, 1], [1, 2, 1]))
+    
