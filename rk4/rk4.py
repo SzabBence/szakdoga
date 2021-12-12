27395485fd1b4c03c282e6f53e956947cc82673d
Szabó,  (Bence Dániel) <szabo.bence@nn.hu>
2021-12-12 14:43:29 +0100

    generalize rk4

--- rk4/rk4.py
+++ rk4/rk4.py
@@ -2,28 +2,28 @@
 # A sample differential equation "dy / dx = (x - y)/2"
 
 
-def rk4_dydx(lepeskoz,
-             kezdeti_pont,
-             kezdeti_ertek,
-             pont) -> float:
+def rk4(lepeskoz: float,
+        kezdeti_pont: float,
+        kezdeti_ertek: float,
+        pont: float,
+        func: str) -> float:
     """
 
     :param lepeskoz: h
     :param kezdeti_pont: x_0
     :param kezdeti_ertek: y_0
     :param pont: x
+    :param func: fuggveny neve, amivel a problemat megoldjuk
     :return: RK4 kozelitese a feladatnnak
     """
     n = int((pont - kezdeti_pont) / lepeskoz)
 
     y = kezdeti_ertek
     for i in range(1, n + 1):
-
-        k1 = lepeskoz * dydx([kezdeti_pont, y])
-        k2 = lepeskoz * dydx([kezdeti_pont + 0.5 * lepeskoz, y + 0.5 * k1])
-        k3 = lepeskoz * dydx([kezdeti_pont + 0.5 * lepeskoz, y + 0.5 * k2])
-        k4 = lepeskoz * dydx([kezdeti_pont + lepeskoz, y + k3])
-
+        k1 = lepeskoz * globals()[func]([kezdeti_pont, y])
+        k2 = lepeskoz * globals()[func]([kezdeti_pont + 0.5 * lepeskoz, y + 0.5 * k1])
+        k3 = lepeskoz * globals()[func]([kezdeti_pont + 0.5 * lepeskoz, y + 0.5 * k2])
+        k4 = lepeskoz * globals()[func]([kezdeti_pont + lepeskoz, y + k3])
         y = y + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
 
         kezdeti_pont = kezdeti_pont + lepeskoz
@@ -45,4 +45,4 @@ if __name__ == "__main__":
     y = 1
     x = 2
     h = 0.2
-    print('A fuggveny erteke az x=' + str(x) + " pontban: " + str(rk4_dydx(h, x0, y, x)))
+    print('A fuggveny erteke az x=' + str(x) + " pontban: " + str(rk4(h, x0, y, x, "dydx")))
