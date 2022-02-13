fb20f5e05895ddbc5d340fab170f257d0417b2eb
SzabBence <szabobencedaniel98@gmail.com>
2022-02-13 21:48:50 +0100

    save von mises code

---
+++ sajatertek_sajatvektor_scripts/eigenvals.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def von_mises(A,
+              iteracio_szam: int):
+    """
+    
+    :param A: matrix
+    :param iteracio_szam: mennyi szimulaciot szeretnenk futtatni
+    :return: dominans sajat vektor kozelitese
+    """
+    # Feltesszuk, hogy nincs tudasunk, hogy milyen lehet a dominans sajatvektor
+    # ezert veletlen vektorral dolgozunk, illetve ezzel kisebb az eselye annak,
+    # hogy b es A szorzata 0 lesz, azaz, hogy ortoginalisak egymasra
+    b_k = np.random.rand(A.shape[1])
+
+    for iterval in range(iteracio_szam):
+        # elvegezzuk a veletlen vektor es a matrix szorzasat
+        b_k0 = np.dot(A, b_k)
+
+        # letrehozzuk a normalt vektort
+        norm = np.linalg.norm(b_k0)
+
+        # normalizaljuk a vektort a normalt vektorral
+        b_k= b_k0 / norm
+
+        #  ezt a harom lepest folytatjuk
+    return b_k
+
+
+    
+matrix = np.array([[1, 0], [0, 1]])
+print(von_mises(matrix, 1000))
