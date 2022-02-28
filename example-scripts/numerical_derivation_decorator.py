86d5b81cc8ecfcc7cbcb207f47be39936cdf1752
SzabBence <szabobencedaniel98@gmail.com>
2022-02-28 14:16:05 +0100

    blacked file for niceness

--- example-scripts/numerical_derivation_decorator.py
+++ example-scripts/numerical_derivation_decorator.py
@@ -1,15 +1,16 @@
-import numpy as np
-
 def numerical_derivation(func):
     def calculation(point):
         h = 0.001
-        return (func(point+h) - func(point-h)) / h
+        return (func(point + h) - func(point - h)) / h
+
     return calculation
+
 
 @numerical_derivation
 def fuggveny(point):
 
     return point * 2
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(fuggveny(1))
