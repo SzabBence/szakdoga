5dd4a5bbff19e222899270d5d5e2345715db3acb
Szabó,  (Bence Dániel) <szabo.bence@nn.hu>
2022-04-27 08:57:55 +0200

    rk4 multidim update, still not functioning

--- rk4/rk4_multidim.py
+++ rk4/rk4_multidim.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def rk4_multidimensional(func: str,
+def rk4_multidimensional(func_list: list,
                          t_0: float,
                          pont: float,
                          x_0: np.array,
@@ -10,14 +10,15 @@ def rk4_multidimensional(func: str,
     t = t_0
     t_end = pont
     h = lepeskoz
+
     while t < t_end:
-        k_0 = globals()[func](t, x)
+        k_0 = func_wrapper(func_list, t, x)
         x_1 = x + h / 2 * k_0
-        k_1 = globals()[func](t + h / 2, x_1)
+        k_1 = func_wrapper(func_list, t + h / 2, x_1)
         x_2 = x + h/2 * k_1
-        k_2 = globals()[func](t +h/2, x_2)
+        k_2 = func_wrapper(func_list, t + h / 2, x_2)
         x_3 = x + h * k_2
-        k_3 = globals()[func](t + h, x_3)
+        k_3 = func_wrapper(func_list, t + h, x_3)
         x = x + h/6 * (k_0 + 2*k_1 + 2*k_2 + k_3)
         t = t + h
 
@@ -31,23 +32,23 @@ def f2(t: float,x: np.array) -> float:
     x_1 = x[0]
     x_2 = x[1]
 
-    return -2 * x_1 + 3 * x_2 + t
+    return -2 * x_1 + 3 * x_2 - np.cos(3*t)
 
 def func_wrapper(func_list : list,
                  t:float,
                  X: np.array):
-    result = np.array([])
+    result = np.empty([len(func_list), 1], dtype=float)
+    index = 0
     for func in func_list:
-        if result.size == 0:
-            result = globals()[func](t, X)
-        else:
-            result.append(result, globals()[func](t, X), axis=0)
-
+        result[index] = globals()[func](t, X)
+        index += 1
     return result
 if __name__ == "__main__":
-    x_0 = np.array([[2],[-3]])
+    x_0 = np.array([[2], [-3]])
     t_0 = 0
     pont = 1
-    h = 0.001
+    h = 0.00001
     func_list = ["f1", "f2"]
-    print(rk4_multidimensional("reduced_to_ode",t_0,pont,x_0,h))
+    z = func_wrapper(func_list,t_0,x_0)
+    print(rk4_multidimensional(func_list, t_0, pont, x_0, h))
+    print(1)
