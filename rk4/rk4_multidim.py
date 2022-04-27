5bf34f1641c4eb33d31d3fb3566d9c5338969b17
Szabó,  (Bence Dániel) <szabo.bence@nn.hu>
2022-04-28 00:55:25 +0200

    save tex

--- rk4/rk4_multidim.py
+++ rk4/rk4_multidim.py
@@ -13,13 +13,13 @@ def rk4_multidimensional(func_list: list,
 
     while t < t_end:
         k_0 = func_wrapper(func_list, t, x)
-        x_1 = x + h / 2 * k_0
+        x_1 = x + h/2 * k_0
         k_1 = func_wrapper(func_list, t + h / 2, x_1)
         x_2 = x + h/2 * k_1
         k_2 = func_wrapper(func_list, t + h / 2, x_2)
-        x_3 = x + h * k_2
+        x_3 = x +  k_2
         k_3 = func_wrapper(func_list, t + h, x_3)
-        x = x + h/6 * (k_0 + 2*k_1 + 2*k_2 + k_3)
+        x = x + h/6 * (k_0 + 2 * k_1 + 2 * k_2 + k_3)
         t = t + h
 
     return x
@@ -32,8 +32,15 @@ def f2(t: float,x: np.array) -> float:
     x_1 = x[0]
     x_2 = x[1]
 
-    return -2 * x_1 + 3 * x_2 - np.cos(3*t)
+    return -2 * x_1 + 3 * x_2 - t
 
+def f3(t: float,x: np.array):
+
+    return x[1]
+
+def f4(t: float,x: np.array):
+
+    return -x[1] - np.sin(x[0]) + np.sin(t)
 def func_wrapper(func_list : list,
                  t:float,
                  X: np.array):
@@ -47,8 +54,14 @@ if __name__ == "__main__":
     x_0 = np.array([[2], [-3]])
     t_0 = 0
     pont = 1
-    h = 0.00001
+    h = 0.0001
     func_list = ["f1", "f2"]
-    z = func_wrapper(func_list,t_0,x_0)
     print(rk4_multidimensional(func_list, t_0, pont, x_0, h))
     print(1)
+
+    # x_0 = np.array([[0], [1]])
+    # t_0 = 0
+    # h = 0.001
+    # pont = 0.2
+    # flist = ["f3", "f4"]
+    # print(rk4_multidimensional(flist,t_0,pont,x_0,h))
